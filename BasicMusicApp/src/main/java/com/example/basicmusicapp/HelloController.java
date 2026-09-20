a0b5f0949d69ab34cfc2848f62ffeb8837f9f427
Bux <kamranbux02@gmail.com>
2026-09-20 18:35:32 +0100

    basic functionality, had to use jlayer cause other stuff wasnt working

--- BasicMusicApp/src/main/java/com/example/basicmusicapp/HelloController.java
+++ BasicMusicApp/src/main/java/com/example/basicmusicapp/HelloController.java
@@ -2,13 +2,40 @@ package com.example.basicmusicapp;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.stage.FileChooser;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import java.io.File;
+import javazoom.jl.player.Player;
+import java.io.FileInputStream;
+import java.io.IOException;
+import javazoom.jl.decoder.JavaLayerException;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+    private Player player;
 
     @FXML
-    protected void onPlayButtonClick() {
-        welcomeText.setText("Playing...");
+    protected void onChooseMusicClick(ActionEvent event) throws IOException {
+        FileChooser chooser = new FileChooser();
+        Button button = (Button) event.getSource();
+        File selectedFile = chooser.showOpenDialog(button.getScene().getWindow());
+        if (selectedFile != null) {
+            if (player != null) {
+                player.close();
+            }
+            FileInputStream inputStream = new FileInputStream(selectedFile);
+            player = new Player(inputStream);
+            Thread thread = new Thread(() -> {
+                try {
+                    player.play();
+                    player = null;
+                } catch (JavaLayerException e) {
+                    e.printStackTrace();
+                }
+            });
+            thread.start();
+        }
     }
 }
--- BasicMusicApp/src/main/java/module-info.java
+++ BasicMusicApp/src/main/java/module-info.java
@@ -2,7 +2,7 @@ module com.example.basicmusicapp {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.media;
-
+    requires jlayer;
 
     opens com.example.basicmusicapp to javafx.fxml;
     exports com.example.basicmusicapp;
